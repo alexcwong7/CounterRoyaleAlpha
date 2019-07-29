f21495cf6a8a46e3d7fd9543a55532b0fc0950ae
alexwong7 <alexwong@email.arizona.edu>
2019-07-28 22:45:20 -0700

    https

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var express = require("express");
+/*var express = require("express");
 var app = express();
 var request = require("request");
 
+const fs = require('fs');
+const https = require('https');
 
+const httpsOptions = {
+  cert: fs.readFileSync('./ssl/counterroyale_com.crt'),
+  ca: fs.readFileSync('./ssl/counterroyale_com.ca-bundle'),
+  key: fs.readFileSync('./ssl/counterroyale_com.p7b')
+};
+
+const httpsServer = https.createServer(httpsOptions, app);
 
 // Routing
 app.use(express.static("public"));
@@ -29,27 +38,32 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running");
-});
+// app.listen(process.env.PORT || 3000, () => {
+//   console.log("Server is running");
+// });
 
-// USING REQUEST-PROMISE (+new syntax)
-/*var rp = require("request-promise");
+httpsServer.listen(443, 'counterroyale.com');
 
-var options = {
-    method: 'GET',
-    url: 'https://v3-beta.royaleapi.com/player/2GQCV0G9',
-    headers: {
-        auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjcwMSwiaWRlbiI6IjE3ODQ0MTkxMjQxMDE3NzUzNyIsIm1kIjp7InVzZXJuYW1lIjoiRmFuY3lOYW1lIiwia2V5VmVyc2lvbiI6MywiZGlzY3JpbWluYXRvciI6IjI1NjgifSwidHMiOjE1NjQyNTU2ODQyNTJ9.VkWIPXQxdSnEdFinB27P8IIThAKnFPH22vN1O2Z14Pk'
-    }
+*/
+
+const fs = require("fs");
+const https = require('https');
+
+const hostname = 'counterroyale.com';
+const port = 443;
+
+const httpsOptions = {
+  cert: fs.readFileSync('./ssl/counterroyale_com.crt'),
+  ca: fs.readFileSync('./ssl/counterroyale_com.ca-bundle'),
+  key: fs.readFileSync('./ssl/server.key')
 };
 
-rp(options)
-    .then((body) => {
-        const data = JSON.parse(body);
-        console.log(data);
-    })
-    .catch((err) => {
-        console.log("Error!", err);
-    });
-*/
+const httpsServer = https.createServer(httpsOptions, (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello from the HTTPS server</h1>');
+});
+
+httpsServer.listen(process.env.PORT || 3001, () => {
+  console.log("Server is running");
+});
