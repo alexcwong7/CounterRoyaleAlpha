e6ab313fc134afb78bedd193e7b1fd2d6bc4d03e
alexwong7 <alexwong@email.arizona.edu>
2019-08-05 23:04:08 -0700

    Player search beta, animations and styling

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ const httpServer = http.createServer(app);
 const httpsServer = https.createServer(httpsOptions, app);
 
 // REDIRECT to secure protocol (UNCOMMENT WHEN PUBLISHING!)
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
 	if(req.protocol === 'http') {
 		res.redirect(301, `https://${req.headers.host}${req.url}`);
 	}
 	next();
-});
+});*/
 
 // Routing
 app.use(express.static("public"));
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   // API Request
@@ -44,18 +44,80 @@ app.get("/", (req, res) => {
   request(options, (error, response, body) => {
       if(!error && response.statusCode == 200) {
           var data = JSON.parse(body);
-          console.log(data.cards);
           res.render("index.ejs", {data:data.cards});
       }
   });
+});
 
+app.get("/decks", (req, res) => {
+
+  res.render("decks.ejs");
+});
+
+app.get("/players", (req, res) => {
+  var query = req.query.tag;
+  if(query === undefined) {
+    res.render("players.ejs");
+  }
+  else {
+    //query = query.split('#').join("").toUpperCase();
+    var options = {
+        method: 'GET',
+        url: 'https://v3-beta.royaleapi.com/player/'+query,
+        headers: {
+            auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjcwMSwiaWRlbiI6IjE3ODQ0MTkxMjQxMDE3NzUzNyIsIm1kIjp7InVzZXJuYW1lIjoiRmFuY3lOYW1lIiwia2V5VmVyc2lvbiI6MywiZGlzY3JpbWluYXRvciI6IjI1NjgifSwidHMiOjE1NjQyNTU2ODQyNTJ9.VkWIPXQxdSnEdFinB27P8IIThAKnFPH22vN1O2Z14Pk'
+        }
+    };
+
+    request(options, (error, response, body) => {
+        if(!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("players.ejs", {data: data});
+        }
+        else {
+          res.send("Player not found!");
+        }
+    });
+  }
+});
+
+/*app.get("/playersearch", (req, res) => {
+  var query = req.query.search;
+  if(typeof query === undefined) {
+    console.log('undefined playersearch');
+    res.render("players.ejs");
+  }
+  else {
+    console.log(query);
+
+    res.redirect("/players/" + query);
+  }
+});*/
+
+app.get("/players/:query", (req, res) => {
+  //var query = req.query.search;
+  var query = req.params.query;
+  console.log("my query!!" +query);
+  var url = "https://v3-beta.royaleapi.com/player/" + query;
+  console.log("/players/*");
+  request(url, (error, response, body) => {
+      if(!error && response.statusCode == 200) {
+          var data = JSON.parse(body);
+          console.log(data["name"]);
+          res.render("players.ejs", {data: data});
+      }
+      else {
+        res.send("Player not found!");
+      }
+  });
+  //res.render("players.ejs");
 });
 
 // Start server GLOBALLY (UNCOMMENT WHEN PUBLISHING!)
-httpServer.listen(httpPort, hostname);
-httpsServer.listen(httpsPort, hostname);
+//httpServer.listen(httpPort, hostname);
+//httpsServer.listen(httpsPort, hostname);
 
 // Start server LOCALLY
-/*app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
-});*/
+});
